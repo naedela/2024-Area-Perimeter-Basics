76078c60edc9d49b3d333dac07829d7508f0b5c5
naedela <161780718+naedela@users.noreply.github.com>
2024-03-04 14:30:24 +1300

    Variable Basis

---
+++ P_02_variables.py
@@ -0,0 +1,13 @@
+# Code to learn about variables...
+
+# Variables can be a string (note the speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_num = 7
+num_string = "5"
+
+print(fav_num *2)
+print(num_string *5)
+
+
